30134ad6aa7975e9339d656964d22ba562faa30f
Arshath01 <ahamedarshath35@gmail.com>
2023-09-04 21:21:43 +0530

    calc

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,43 @@
+import {useState} from 'react';
 
-import InputField from './components/input';
-import Navbar from "./components/Navbar";
-import Section from './components/section';
+function App(){
+  const [display,setDisplay] = useState('');
+  const arr = ['0','1','2','3','4','5',
+  '6','7','8','9','+','*','-',
+  '/','=','C'];
 
-import './App.css';
-import { useState } from 'react';
+  function handleButton(buttons){
+    if(buttons === "="){
+      // 0 === '0'
+      // console.log(eval(display));
+      const result = eval(display);
+      setDisplay(result);
+    }
+    else if(buttons === 'C'){
+      setDisplay('');
+    }
+    else{
+      setDisplay(prevState => prevState + buttons);
+    }
+  }
 
-function App() {
-
-  const [bool,setBool] = useState(false);
- return (
-  <div>
-    <Navbar/>
-    <button onClick={()=>setBool(true)}>
-      click me to show section...
-    </button>
-
-    <button onClick={()=>setBool(false)}>
-      click me to hide section...
-    </button>
-
-    {bool && <Section/>}
-    {/* <InputField/> */}
-    
-  </div>
- );
+  return(
+    <div className="calc">
+        <h1>Calculator</h1>
+        <div className='display'>
+          <h2>{display}</h2>
+        </div>
+        <div className='buttons_wrap'>
+          {
+            arr.map((buttons) => (
+              <button id='button' onClick={()=>handleButton(buttons)}>
+                {buttons}
+              </button>
+            ))
+          }
+        </div>
+    </div>
+  )
 }
 
 export default App;
